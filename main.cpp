3b519f3b7b9f85376cdee60ec87ba9820b31ae2a
MarvinKatapult <ibingmarvin@gmail.com>
2023-09-10 19:00:49 +0200

    refactor

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 
+// Is number a primenumber?
+bool checkPrime(int number) {
+    
+    for (int j = 2; j < number; j++) {
+
+        if (number % j == 0) {
+            return false;
+        }
+        if (j > number / 2) {
+            return true;
+        }
+
+    }
+}
+
+// Get amount of primenumbers from 0 to a given value
 int getPrimes(int toNumber) {
-    // eq 2 because 2 is Prime
+    // eq 1 because 2 is Prime
+
     int result = 1;
     
     for (int i = 3; i < toNumber; i++) {
-        // std::cout << i << " = i \n\n";
-        for (int j = 2; j < i; j++) {
-            // std::cout << j << " = j \n";
-            if (i % j == 0) {
-                // std::cout << "Is not Prime: Break\n";
-                break;
-            }
-
-            if (j > i / 2) {
-                
-                // std::cout << "Found Prime: \n" << i << "\n\n";
-                // std::cout << "Current result: " << result + 1 << "\n";
-                result++;
-                break;
-            }
+        if (checkPrime(i) == true) {
+            result++;
         }
     }
     return result;
@@ -37,7 +41,6 @@ int main() {
     result = getPrimes(input);
     ratio = ((float)result / (float)input) * 100;
     
-    std::cout <<  "There are " << result << " Primes to " << input << "\nThats a " << ratio << "% Ratio.";
+    std::cout <<  "There are " << result << " Primes in " << input << "\nThats a " << ratio << "% Ratio.";
     return 0;
 }
-
